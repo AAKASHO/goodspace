15bb63b5b8a02d9915b7343ff993c991ae7e5d66
akash kumar <iamaakashkumar789@gmail.com>
2023-12-31 01:28:53 +0530

    final

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import * as Server from 'socket.io';
 
 const app = express();
 const corsConfig = {
-  origin: process.env.BASE_URL,
+  origin: 'https://65906d264abcd69a33ff6f6b--neon-praline-aede82.netlify.app/',
   credentials: true,
 };
 
